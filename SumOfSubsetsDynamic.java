cff4b65c15c5c9fdde71ca59a0b4805195e837bc
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-23 02:03:21 -0800

    core algorithm rewrite

--- SumOfSubsetsDynamic.java
+++ SumOfSubsetsDynamic.java
@@ -16,7 +16,7 @@ public class SumOfSubsetsDynamic {
     public static String toString(int[] arr) {
         String str = "[";
 
-        for(int i = 0; i < arr.length-1; i++)         {
+        for(int i = 0; i < arr.length-1; i++) {
             str += arr[i] + ", ";
         }
 
@@ -24,38 +24,39 @@ public class SumOfSubsetsDynamic {
         return str;
     }
     
-    public static boolean isSubsetSum(int arr[], int n, int sum) {
+    public static boolean isSubsetSum(int givenArr[], int n, int sum)
+    {
         boolean matrix[][] = new boolean[sum + 1][n + 1];
-
-        // sets top row to false
-        for (int i = 0; i <= sum; i++) {
-            matrix[i][0] = false;
-        }            
         
-        // sets first column to true
-        for (int i = 0; i <= n; i++) {
-            matrix[0][i] = true;
+        for (int i = 0; i <= sum; i++)
+        {
+            matrix[i][0] = false; // fill first row with false
         }
         
+        for (int i = 0; i <= n; i++) {
+            matrix[0][i] = true; // fill first column with true
+        }
+
         for (int i = 1; i <= sum; i++) {
             for (int j = 1; j <= n; j++) {
                 matrix[i][j] = matrix[i][j - 1];
-                if (i >= arr[j - 1]) {
-                    matrix[i][j] = matrix[i][j] || matrix[i - arr[j - 1]][j - 1];
-                }                    
+                if (i >= givenArr[j - 1] && matrix[i - givenArr[j - 1]][j - 1]) {
+                    matrix[i][j] = true;
+                }
             }
-
         }
-        
-        // prints matrix
-        for (int i = 0; i <= sum; i++) {
+ 
+        // print boolean array
+        for (int i = 0; i <= sum; i++)
+        {
             for (int j = 0; j <= n; j++) {
                 System.out.print(matrix[i][j] + " ");
-            }   
-            System.out.println();             
+            }
+            System.out.println();
         }
         System.out.println();
- 
+        
         return matrix[sum][n];
     }
+
 }
