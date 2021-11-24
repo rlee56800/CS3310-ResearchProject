7f5e4e5fad37c819e1e1d0cfb14e1716b7a5cb5e
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-23 22:18:33 -0800

    aesthetic changes

--- SumOfSubsetsDynamic.java
+++ SumOfSubsetsDynamic.java
@@ -4,7 +4,7 @@ public class SumOfSubsetsDynamic {
         int k = 6;//9;
         int n = arr.length;
 
-        if (isSubsetSum(arr, n, k) == true) {
+        if(isSubsetSum(arr, n, k) == true) {
             System.out.println("The set " + toString(arr) + " contains the sum k = " + k);
         }
         else {
@@ -24,63 +24,45 @@ public class SumOfSubsetsDynamic {
         return str;
     }
     
-    static boolean isSubsetSum(int set[], int n, int sum)
-
-    {
-        boolean subset[][] = new boolean[n + 1][sum + 1];
+    public static boolean isSubsetSum(int set[], int n, int k) {
+        boolean matrix[][] = new boolean[n + 1][k + 1];
  
-
-        for (int i = 0; i <= sum; i++)
-
-            subset[0][i] = false;
+        // sets first row false
+        for(int i = 0; i <= k; i++) {
+            matrix[0][i] = false;
+        }
         
-        // If sum is 0, then answer is true
+        // sets first column true
+        for(int i = 0; i <= n; i++) {
+            matrix[i][0] = true;
+        }
 
-        for (int i = 0; i <= n; i++)
-
-            subset[i][0] = true;
- 
-
- 
-
-        // Fill the subset table in bottom
-
-        // up manner
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= sum; j++) {
-
-                /*subset[i][j] = subset[i-1][j];
-
-                if (j >= set[i - 1])
-                {
-                    subset[i][j] = subset[i][j] || subset[i - 1][j - set[i - 1]];
-                }*/
-
+        // logic
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= k; j++) {
                 if(set[i-1] > j) {
-                    subset[i][j] = subset[i-1][j];
+                    // if element is greater than element, take value of above row (same column)
+                    matrix[i][j] = matrix[i-1][j];
                 } else {
-                    subset[i][j] = subset[i-1][j] || subset[i - 1][j - set[i - 1]];
+                    // else take either above row (same column) or above row, element columns left
+                    // if element == sum, gets value from sum = 0
+                    matrix[i][j] = matrix[i-1][j] || matrix[i - 1][j - set[i - 1]];
                 }
             }
 
         }
  
 
-        // print boolean array
-        for (int i = 0; i <= n; i++)
-        {
-            for (int j = 0; j <= sum; j++) {
-                System.out.print(subset[i][j] + " ");
+        // print matrix
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= k; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println();
- 
 
-        return subset[n][sum];
-
+        return matrix[n][k];
     }
 
 }
